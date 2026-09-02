d32248c44a6e01648cc451360b099625b92a8c8f
Hossein Darestani <45485005+hsdarestani@users.noreply.github.com>
2026-09-02 15:12:12 +0330

    Route Zibal payments through HamoonCloud origin

---
+++ zibal-referer-bootstrap.js
@@ -0,0 +1,23 @@
+'use strict';
+
+function applyZibalRefererPatches(source) {
+  const direct = 'const payUrl = `https://gateway.zibal.ir/start/${trackId}`;';
+  const intermediary = 'const payUrl = `https://hamooncloud.ir/payment/start/${trackId}`;';
+
+  const count = String(source).split(direct).length - 1;
+  if (count !== 1) {
+    const err = new Error(`ZIBAL_REFERER_PAYMENT_URL_MARKER_MISMATCH:${count}`);
+    err.code = 'ZIBAL_REFERER_PAYMENT_URL_MARKER_MISMATCH';
+    throw err;
+  }
+
+  const patched = String(source).replace(direct, intermediary);
+  if (!patched.includes(intermediary) || patched.includes(direct)) {
+    const err = new Error('ZIBAL_REFERER_PATCH_FAILED');
+    err.code = 'ZIBAL_REFERER_PATCH_FAILED';
+    throw err;
+  }
+  return patched;
+}
+
+module.exports = { applyZibalRefererPatches };
