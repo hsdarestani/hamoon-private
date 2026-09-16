14017ef4589384e9cd84e018971d46dbce0e5aee
Hossein Darestani <45485005+hsdarestani@users.noreply.github.com>
2026-09-16 19:19:27 +0330

    Use Hetzner power-state barrier in ops recovery

--- scripts/ops-server-by-owner-ip.js
+++ scripts/ops-server-by-owner-ip.js
@@ -5,6 +5,8 @@ require('dotenv').config();
 const db = require('../db');
 const datacenters = require('../datacenters');
 const cloud = require('../cloud-api');
+const { installHetznerPowerStateBarrier } = require('../services/hetzner-power-state-barrier');
+installHetznerPowerStateBarrier();
 const cleanIp = require('../services/hetzner-clean-ip-change');
 
 function sleep(ms) { return new Promise(resolve => setTimeout(resolve, ms)); }
